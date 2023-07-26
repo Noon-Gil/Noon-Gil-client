0062c133858daeb1307709dabcb7fe9151c5ba12
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-26 16:17:30 +0900

    feat: detectpage 다양한 기능 추가

--- src/pages/DetectPage.js
+++ src/pages/DetectPage.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Toast from 'react-native-easy-toast';
+import { Audio } from 'expo-av';
 import { Camera } from 'expo-camera';
+import * as Haptics from 'expo-haptics';
+import * as ImageManipulator from 'expo-image-manipulator';
 import * as Speech from 'expo-speech';
-
 
 const DetectPage = () => {
   const [hasPermission, setHasPermission] = useState(null);
+  const [recording, setRecording] = useState();
   const cameraRef= useRef(null);
   const toastRef = useRef(); // toast ref 생성
   const windowHeight = Dimensions.get("window").height;
@@ -22,21 +25,72 @@ const DetectPage = () => {
     toastRef.current.show('press detected');
   }, []);
 
+  const takeVoice = async () => {
+    Haptics.notificationAsync(
+      Haptics.NotificationFeedbackType.Success
+    );
+    try {
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+      setRecording(recording);
+      await setTimeout(async () => {
+        Haptics.notificationAsync(
+          Haptics.NotificationFeedbackType.Success
+        );
+        console.log('Stopping recording..');
+        setRecording(undefined);
+        await recording.stopAndUnloadAsync();
+        const uri = recording.getURI();
+        console.log('Recording stopped and stored at', uri);
+        console.log('Loading Sound');
+        const sound = new Audio.Sound();
+        await sound.loadAsync({ uri: uri });
+        await sound.replayAsync();
+
+        const formData = new FormData();
+        formData.append('name', {
+          name: "sound",
+          type: "audio/m4a",
+          uri: uri,
+        });
+        const headers = { "Content-Type": "multipart/form-data" };
+        const apiUrl = "http://dht.social";
+        console.log("uploading...");
+        const response = await axios.post(`${apiUrl}/stt/selection`, formData, {
+          headers: headers,
+          transformRequest: formData => formData,
+        });
+        console.log("upload result: ", response.data.content);
+        Speech.speak(response.data.content);
+      }, 4000);
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
   const takePhoto = async () => {
+    Haptics.notificationAsync(
+      Haptics.NotificationFeedbackType.Success
+    );
+
     if(cameraRef){
       const data= await cameraRef.current.takePictureAsync({
         quality:1,
         exif:true
       });
+      const manipResult = await ImageManipulator.manipulateAsync(
+        data.uri,
+        [{ rotate: 90 }, { flip: ImageManipulator.FlipType.Vertical }, { flip: ImageManipulator.FlipType.Horizontal }],
+        { compress: 0.5, format: ImageManipulator.SaveFormat.JPEG }
+      );
       console.log('data: ', data.uri);
       const image = {
         name: "image",
         type: "image/jpg",
-        uri: data.uri,
+        uri: manipResult.uri,
       }
       const formData = new FormData();
       formData.append('name', image);
-      console.log("formData: ", formData);
       const headers = {
         "Content-Type": "multipart/form-data",
       };
@@ -46,15 +100,22 @@ const DetectPage = () => {
         headers: headers,
         transformRequest: formData => formData,
       });
-      console.log("upload response: ", console.log(response));
+      console.log("upload result: ", response.data.content);
+      Speech.speak(response.data.content);
     }
   }
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
       setHasPermission(status === 'granted');
     })();
+    Speech.speak('안녕하세요, 눈길 물체인식 입니다. 화면을 탭하면 이미지를 묘사해 볼게요');
   }, []);
 
   if (hasPermission === null) {
@@ -72,7 +133,8 @@ const DetectPage = () => {
         ref={cameraRef}>
         <TouchableOpacity onPress={() => {
           showCopyToast();
-          takePhoto();
+          takeVoice();
+          // takePhoto();
         }} style={styles.cameraContiner}>
         </TouchableOpacity>
       </Camera>
